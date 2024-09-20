a811e400e15163dd167743de67aa9b16491ebb7e
Shiavm Dhamejani <74037174+dhamejanishivam@users.noreply.github.com>
2024-09-21 03:41:15 +0530

    Add files via upload

---
+++ files/script.js
@@ -0,0 +1,44 @@
+
+var typed = new Typed(".text", {
+    strings: ["Programming" , "Cybersecurity" , "Web Development"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
+
+
+
+
+function typer(txt,speed,elementid){
+    var i = 0;
+    function type(){
+        if(i<txt.length){
+            document.getElementById(elementid).innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(type,speed);
+        }
+    }
+    type();
+}
+
+
+
+function copy(){
+    navigator.clipboard.writeText("+91 9691520339");
+    elementid = "phoneNumber";
+    document.getElementById(elementid).innerHTML = '';
+    document.getElementById(elementid).innerHTML = '<i class="bx bxs-phone-call">';
+    typer("Phone Number Copied",30,"phoneNumber");
+};
